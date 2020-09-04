87418a995151540cae54367635127a66ef219ab4
zhuang002 <zhuang001@gmail.com>
2020-09-03 20:28:32 -0400

    change to product

--- src/gitsample2/Gitsample2.java
+++ src/gitsample2/Gitsample2.java
@@ -16,11 +16,11 @@ public class Gitsample2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int sum=0;
-        for (int i=0;i<100;i++) {
-            sum+=i;
+        int product=0;
+        for (int i=0;i<10;i++) {
+            product*=i;
         }
-        System.out.println(sum);
+        System.out.println(product);
     }
     
 }
